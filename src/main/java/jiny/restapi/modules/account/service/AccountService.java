809efc584b139e81d1d71e17ae018a4b9a65482f
jyoung7988 <jyoung7988@naver.com>
2023-04-12 13:02:11 +0900

    오타 수정

--- src/main/java/jiny/restapi/modules/account/service/AccountService.java
+++ src/main/java/jiny/restapi/modules/account/service/AccountService.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-dimport java.util.List;
+import java.util.List;
 import java.util.Optional;
 
 @Service
