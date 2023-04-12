ff6bcdd82d01d73be63ded0ded2875c99b14a4fe
jyoung7988 <jyoung7988@naver.com>
2023-04-12 13:01:38 +0900

    이메일,닉네임 중복 검사

--- src/main/java/jiny/restapi/modules/account/service/AccountService.java
+++ src/main/java/jiny/restapi/modules/account/service/AccountService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+dimport java.util.List;
 import java.util.Optional;
 
 @Service
@@ -38,6 +39,22 @@ public class AccountService implements UserDetailsService {
 
     private Account saveNewAccount(SignUpForm signUpForm) {
         Account account = Account.createAccount(signUpForm.getEmail(), signUpForm.getNickname(), passwordEncoder.encode(signUpForm.getPassword()));
+        validateDuplicatedEmail(account);
+        validateDuplicatedNickname(account);
         return accountRepo.save(account);
+    }
+
+    private void validateDuplicatedEmail(Account account){
+        Account findAccount = accountRepo.findByEmail(account.getEmail());
+        if (findAccount != null) {
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        }
+    }
+
+    private void validateDuplicatedNickname(Account account){
+        Account findAccount = accountRepo.findByNickname(account.getNickname());
+        if (findAccount != null) {
+            throw new IllegalStateException("이미 존재하는 닉네임입니다.");
+        }
     }
 }
