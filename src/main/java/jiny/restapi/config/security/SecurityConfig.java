a26e045dc15ac84f7712f5e5de1b9d8e211ee201
jyoung7988 <jyoung7988@naver.com>
2023-04-18 23:24:50 +0900

    시큐리티 cors 재설정 - spring security를 사용하게 되면 spring MVC에 도달하기 전에 spring security에서 작업을 먼저 처리하기 때문에 설정한 webconfig는 적용이 되지못하고 security가 cors를 차단하여 에러가 발생하였음

--- src/main/java/jiny/restapi/config/security/SecurityConfig.java
+++ src/main/java/jiny/restapi/config/security/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration @EnableWebSecurity
 @RequiredArgsConstructor
@@ -33,6 +36,8 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.GET,"post/**").permitAll()
                 .anyRequest().hasRole("USER")
                 .and()
+                .cors()
+                .and()
                 .addFilterBefore(new JwtAuthFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
@@ -42,4 +47,17 @@ public class SecurityConfig {
         return (web) -> web.ignoring().antMatchers("/api-docs","/swagger-ui/**");
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 }
